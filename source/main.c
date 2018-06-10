850dc642b31f6de29ca4c546f49e47175a027577
Guilherme Cesar dos Santos <gui.gui.guicesar12@hotmail.com>
2018-06-10 20:03:54 -0300

    Updated DT_HASH_SEGMENT Offset

--- source/main.c
+++ source/main.c
@@ -64,7 +64,7 @@ int kpayload(struct thread *td, struct kpayload_args* args){
 	writeCr0(cr0 & ~X86_CR0_WP);
 	
 	//Kexec init
-	void *DT_HASH_SEGMENT = (void *)(kernel_base+ 0xB1D820); // I know it's for 4.55 but I think it will works
+	void *DT_HASH_SEGMENT = (void *)(kernel_base+ 0xB5EF30); // I know it's for 4.55 but I think it will works
 	memcpy(DT_HASH_SEGMENT,kexec, kexec_size);
 
 	void (*kexec_init)(void *, void *) = DT_HASH_SEGMENT;
