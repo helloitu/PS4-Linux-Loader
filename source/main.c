eac9647aeb79307bed5629fae5e9a2921d36b0c6
Guilherme Cesar dos Santos <gui.gui.guicesar12@hotmail.com>
2018-06-10 18:36:11 -0300

    Update main.c

--- source/main.c
+++ source/main.c
@@ -3,7 +3,7 @@
 #include "ps4.h"
 #include "defines.h"
 
-#define    KERN_XFAST_SYSCALL 0x1c0
+#define KERN_XFAST_SYSCALL 0x1C0
 
 #define	CTL_KERN	1	/* "high kernel": proc, limits */
 #define	KERN_PROC	14	/* struct: process entries */
@@ -40,13 +40,13 @@ int kpayload(struct thread *td, struct kpayload_args* args){
 	fd = td->td_proc->p_fd;
 	cred = td->td_proc->p_ucred;
 
-	//Reading kernel_base...
+	//Lendo base do Kernel...
 	void* kernel_base = &((uint8_t*)__readmsr(0xC0000082))[-KERN_XFAST_SYSCALL];
 	uint8_t* kernel_ptr = (uint8_t*)kernel_base;
 	void** got_prison0 =   (void**)&kernel_ptr[0x10986a0];
 	void** got_rootvnode = (void**)&kernel_ptr[0x22c1a70];
 	
-	//Resolve kernel functions...
+	//Redefine funcoes do Kernel...
 	int (*copyout)(const void *kaddr, void *uaddr, size_t len) = (void *)(kernel_base + 0x1ea630);
 	int (*printfkernel)(const char *fmt, ...) = (void *)(kernel_base + 0x436040);
 	int (*copyin)(const void *uaddr, void *kaddr, size_t len) = (void *)(kernel_base + 0x1ea710);
@@ -64,7 +64,7 @@ int kpayload(struct thread *td, struct kpayload_args* args){
 	writeCr0(cr0 & ~X86_CR0_WP);
 	
 	//Kexec init
-	void *DT_HASH_SEGMENT = (void *)(kernel_base+ 0xB1D820); // I know it's for 4.55 but I think it will works
+	void *DT_HASH_SEGMENT = (void *)(kernel_base+ 0xB5EF30); // updated to 5.05 offset
 	memcpy(DT_HASH_SEGMENT,kexec, kexec_size);
 
 	void (*kexec_init)(void *, void *) = DT_HASH_SEGMENT;
@@ -72,13 +72,10 @@ int kpayload(struct thread *td, struct kpayload_args* args){
 	kexec_init((void *)(kernel_base+0x436040), NULL);
 
 	// Say hello and put the kernel base in userland to we can use later
-	printfkernel("PS4 Linux Loader for 5.05 by valentinbreiz\n");
-
+	printfkernel("PS4 Linux Loader for 5.05 by valentinbreiz and update by helloitu\n");
 	printfkernel("kernel base is:0x%016llx\n", kernel_base);
-
 	uint64_t uaddr;
 	memcpy(&uaddr,&args[2],8);
-
 	printfkernel("uaddr is:0x%016llx\n", uaddr);
 
 	copyout(&kernel_base, uaddr, 8);
@@ -93,33 +90,42 @@ int _main(struct thread *td) {
 	initNetwork();
 	initPthread();
 
-	struct sockaddr_in server;
+	//Define here your pc ip for debug messages
+	// for initialize pc server type:
+	//<port> = ipdebug.port
+	// socat TCP-LISTEN:<port>, stdout
+	ipdebug.ip1=192;
+	ipdebug.ip2=168;
+	ipdebug.ip3=0;
+	ipdebug.ip4=113;
+	ipdebug.port=9023; //padronized port
 
+	struct sockaddr_in server;
 	server.sin_len = sizeof(server);
 	server.sin_family = AF_INET;
-	server.sin_addr.s_addr = IP(192, 168, 1, 12);
-	server.sin_port = sceNetHtons(9023);
+	server.sin_addr.s_addr = IP(ipdebug.ip1, ipdebug.ip2, ipdebug.ip3, ipdebug.ip4);
+	server.sin_port = sceNetHtons(ipdebug.port);
 	memset(server.sin_zero, 0, sizeof(server.sin_zero));
 	sock = sceNetSocket("debug", AF_INET, SOCK_STREAM, 0);
 	sceNetConnect(sock, (struct sockaddr *)&server, sizeof(server));
 	int flag = 1;
 	sceNetSetsockopt(sock, IPPROTO_TCP, TCP_NODELAY, (char *)&flag, sizeof(int));
 	
-	printfsocket("Connected!\n");
+	printfsocket("Conectado!\n");
 
 	uint64_t* dump = mmap(NULL, 0x1000, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
-
-	printfsocket("Starting kernel patch...\n");
+	printfsocket("------Iniciando depuracao por socketprint------");
+	printfsocket("Iniciando Patch de Kernel...\n");
 
 	int sRet = syscall(11,kpayload,td,dump);
 
-	printfsocket("Kernel patched, Kexec initialized!\n");
+	printfsocket("Kernel Patcheado!\n");
 
-	printfsocket("Starting PS4 Linux Loader\n");
+	printfsocket("Iniciando Linux\n");
 	
 	usbthing();
 
-	printfsocket("Done!\n");
+	printfsocket("Inicializacao Concluida\n");
 
 	sceNetSocketClose(sock);
 
@@ -130,13 +136,13 @@ int _main(struct thread *td) {
 void usbthing()
 {
 
-	printfsocket("Open bzImage file from USB\n");
+	printfsocket("Abrindo o arquivo bzImage no pendrive \n");
 	FILE *fkernel = fopen("/mnt/usb0/bzImage", "r");
 	fseek(fkernel, 0L, SEEK_END);
 	int kernelsize = ftell(fkernel);
 	fseek(fkernel, 0L, SEEK_SET);
 
-	printfsocket("Open initramfs file from USB\n");
+	printfsocket("Abrindo arquivo initramfs do usb em /mnt/usb0/initramfs.cpio.gz\n");
 	FILE *finitramfs = fopen("/mnt/usb0/initramfs.cpio.gz", "r");
 	fseek(finitramfs, 0L, SEEK_END);
 	int initramfssize = ftell(finitramfs);
@@ -145,9 +151,9 @@ void usbthing()
 	printfsocket("kernelsize = %d\n", kernelsize);
 	printfsocket("initramfssize = %d\n", initramfssize);
 
-	printfsocket("Checks if the files are here\n");
+	printfsocket("Verifica se os arquivos estao ok\n");
 	if(kernelsize == 0 || initramfssize == 0) {
-		printfsocket("no file error im dead");
+		printfsocket("Sem erros de arquivos");
 		fclose(fkernel);
 		fclose(finitramfs);
 		return;
